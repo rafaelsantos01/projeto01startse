6b81e62496d1a2c7c9e47920ba67f8dd77e8a935
Rafael <rafinhapsantos50@gmail.com>
2022-05-01 19:57:17 -0300

    att menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,8 @@
 
 const btnMobile = document.getElementById('btn-mobile');
+
+
+
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
@@ -11,6 +14,24 @@ function toggleMenu(event) {
     } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
+    
 }
+
+function scrollToID(event) {
+    event.preventDefault();
+    const ID = event.currentTarget.getAttribute('href');
+    const target = document.querySelector(ID);
+    target.scrollIntoView({ behavior: "smooth" });
+    // fecha o menu
+    btnMobile.click();
+  }
+  
+  // Seleciona links internos
+  const intLinks = document.querySelectorAll('[href^="#"]');
+  intLinks.forEach(link => {
+    link.addEventListener('click', scrollToID);
+  });
+
+  
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
